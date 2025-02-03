9833b15fda38bda5d91b5da20fe16b6d6b7a75d0
lukakhuza <lukakhuza7@gmail.com>
2025-02-03 03:15:57 -0500

    Cart Page template added.

---
+++ screens/CartAndCheckout/Cart.js
@@ -0,0 +1,71 @@
+import {
+  BackHandler,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import PurpleButton from "../../components/ui/PurpleButton";
+
+function Cart({ navigation }) {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.root}>
+        <View style={styles.content}>
+          <Image
+            style={styles.image}
+            source={require("../../assets/parcel.png")}
+          />
+          <View>
+            <Text style={{ fontSize: 20, marginVertical: 20 }}>
+              Your Cart is Empty
+            </Text>
+          </View>
+          <View>
+            <PurpleButton
+              onPress={() => {
+                navigation.navigate("CategoriesList");
+              }}
+            >
+              Explore Categories
+            </PurpleButton>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default Cart;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: "100%",
+  },
+  header: {
+    fontSize: 17,
+    textAlign: "center",
+  },
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  imageContainer: {
+    justifyContent: "center",
+    width: 200,
+    height: 200,
+    borderColor: "yellow",
+    borderWidth: 4,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
